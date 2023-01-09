b8e1c64e25fdc48d284ddee688f027ac48aaa754
David Pinkerton <david@dpinkerton.com>
2023-01-09 21:05:29 +1000

    Pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(userInputA, userInputB) {
+    
+    let finalSum = 0;
 
+    if (userInputA < 0 || userInputB < 0) {
+        finalSum = "ERROR";
+    } else if (typeof userInputA !== "number" || typeof userInputB !== "number") {
+        finalSum = "ERROR";
+    } else {
+        let userInputs = [userInputA, userInputB].sort();
+        for (i = userInputs[0]; i <= userInputs[1]; i++) {
+            finalSum += i;
+        }
+    }
+
+    return(finalSum);
 };
 
 // Do not edit below this line
