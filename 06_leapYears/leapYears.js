331662f66cd40f985eedad083f231229e6d51e4c
David Pinkerton <david@dpinkerton.com>
2023-01-10 06:45:45 +1000

    Pass 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    let leapOrNot = false;
+    if (yearInput % 4 == 0) {
+        leapOrNot = true;
+    } if (yearInput % 100 == 0) {
+        leapOrNot = false;
+    } if (yearInput % 400 == 0) {
+        leapOrNot = true;
+    }
+    return(leapOrNot);
 };
 
 // Do not edit below this line
