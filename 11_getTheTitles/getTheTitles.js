b3d0e82bd05f1c80d5889b0a8f0e331c41dc1f67
David Pinkerton <david@dpinkerton.com>
2023-01-17 19:39:18 +1000

    Pass 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(inputArray) {
+let key = "title";
+let extractedTitles = inputArray.map(obj => obj[key]);
+return(extractedTitles);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
