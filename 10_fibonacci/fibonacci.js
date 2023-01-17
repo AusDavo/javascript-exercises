7c1594a2295d7dcd478c789881055c1147e42b8d
David Pinkerton <david@dpinkerton.com>
2023-01-17 13:44:06 +1000

    Pass all tests for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputSequenceLength) {
+    const sequenceLength = Number(inputSequenceLength);
+    let fibonacciSequence = [];
+        if(isNaN(sequenceLength) || sequenceLength <= 0) {
+            return ("OOPS");
+        } else {
+            if(sequenceLength > 0) {
+                fibonacciSequence[0] = 1;
+            }
+            if(sequenceLength > 1) {
+                fibonacciSequence[1] = 1;
+            }
+            if(sequenceLength > 2) {
+                for (i = 2; i < sequenceLength; i++) {
+                    fibonacciSequence[i] = fibonacciSequence[i-1] + fibonacciSequence[i-2];
+                }
+            }
+            return (fibonacciSequence[sequenceLength - 1]);
+        }
 };
 
 // Do not edit below this line
